a61d26ede6203761a40b4d7715151ed3ab5f753e
karlami <karla.rivera24@gmail.com>
2020-08-06 20:55:47 -0600

    Update Functions.sql
    getNombreCompleto

--- Functions.sql
+++ Functions.sql
@@ -16,49 +16,25 @@
 
 /*
 Obtiene el nombre completo de una persona al ingresar su cedula.
-param: cedula INT
+param: _cedula INTEGER
 return: nombre completo concatenado
 */
-/*
-CREATE OR REPLACE FUNCTION Presupuesto1()
-RETURNS TABLE (
-	Nombre_etapa varchar (60),
-	nombre_obra varchar(60),
-	Precio_Etapa float
-)
+
+CREATE OR REPLACE FUNCTION getNombreCompleto(_cedula INTEGER)
+RETURNS VARCHAR(255)
 AS $$
+DECLARE NombreCompleto VARCHAR(50);
 BEGIN
-RETURN QUERY
-    SELECT E.nombre, O.nombre_obra,SUM(R.cantidad * M.precio_unitario) as Precio_Etapa
-	FROM Requiere R
-	INNER JOIN Obra O
-	ON R.id_obra = O.id
-	INNER JOIN Material M
-	ON M.codigo = R.codigo_material
-	INNER JOIN Etapa E
-	ON E.id = R.id_etapa
-	GROUP BY O.id, E.nombre;
+    SELECT
+        CONCAT((SELECT persona.nombre FROM persona
+		WHERE _cedula=persona.cedula),' ',
+            (SELECT persona.primerApellido FROM persona
+		WHERE _cedula=persona.cedula),' ',
+            (SELECT persona.segundoApellido FROM persona
+		WHERE _cedula=persona.cedula)) INTO NombreCompleto;
+	RETURN NombreCompleto;
 END; $$
 LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION Planilla()
-RETURNS TABLE (
-	nombre_obra varchar (60),
-	id int,
-	nombre varchar(60),
-	semana int,
-	pago_semana float
-)
-AS $$
-BEGIN
-	RETURN QUERY
-	SELECT	O.nombre_obra, O.id, E.nombre,L.semana,(E.pago_hora * L.horas_laboradas) as Pago_Semana
-	FROM Labora_en L
-	INNER JOIN Empleado E
-	ON L.id_empleado = E.id
-	INNER JOIN Obra O
-	ON O.id = L.id_obra;
-END;
-$$ LANGUAGE 'plpgsql';
+-- SELECT getNombreCompleto(200765074);
 
-*/
