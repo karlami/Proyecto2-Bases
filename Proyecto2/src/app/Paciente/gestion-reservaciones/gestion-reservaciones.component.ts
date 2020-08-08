d1ba0a93052ec85a04f8158bfce21b5558da7888
fiorellaD53 <fdelgado53@estudiantec.cr>
2020-08-08 07:22:36 -0600

    reservacion

--- Proyecto2/src/app/Paciente/gestion-reservaciones/gestion-reservaciones.component.ts
+++ Proyecto2/src/app/Paciente/gestion-reservaciones/gestion-reservaciones.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Reservacion } from 'src/app/Modelos/reservacion.model';
+import { Reservacionp } from 'src/app/Modelos/reservacionp.model';
+import { ReservacionManagementService } from 'src/app/Servicios/reservacion-management.service';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-gestion-reservaciones',
@@ -7,9 +14,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionReservacionesComponent implements OnInit {
 
-  constructor() { }
+  reservacionList: Reservacion[];
+  reservacionForm: NgForm;
+  submitted = false;
+  reservacionn: Reservacionp;
+  reservacion: Reservacion;
+  closeResult = '';
+
+  constructor( public service: ReservacionManagementService, private formBuilder: FormBuilder,
+              private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    this.service.getReservacion();
+    this.generateFormU();
   }
 
+  // metodo para generar el formulario para el put
+  generateFormU(reservacionForm?: NgForm) {
+    if (reservacionForm != null) {
+      reservacionForm.reset();
+    }
+    this.reservacionn = {
+        numerocama: undefined,
+        idequipo: undefined,
+        idsalon: undefined,
+        uci: false
+    };
+
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  // metodo para el post
+  onSubmit(camaForm: NgForm) {
+    this.service.postReservacion(this.reservacionn);
+    console.log(this.reservacionn);
+    this.generateFormU();
+    window.location.reload();
+  }
+
+
 }
+
+
