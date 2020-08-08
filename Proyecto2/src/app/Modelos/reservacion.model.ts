f8abbdf65bd20b96676e553ef3a8ec5dfc12498c
fiorellaD53 <fdelgado53@estudiantec.cr>
2020-08-08 06:33:21 -0600

    Reservacion

--- Proyecto2/src/app/Modelos/reservacion.model.ts
+++ Proyecto2/src/app/Modelos/reservacion.model.ts
@@ -2,7 +2,7 @@ export class Reservacion {
     idreservacion: number;
     idpaciente: number;
     fechaingreso: Date;
-    procedimientos: string;
     fechasalida: Date;
+    idcama: number;
 
 }
